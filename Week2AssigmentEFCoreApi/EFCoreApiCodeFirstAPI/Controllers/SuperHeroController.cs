93a0afcf143c6bfe7c7921a6d87b161f68c4f057
Yasin GÜL <89739419+yasingul@users.noreply.github.com>
2022-06-18 11:28:47 +0300

    Week2API

---
+++ Week2AssigmentEFCoreApi/EFCoreApiCodeFirstAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreApiCodeFirst.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>
+        {
+            new SuperHero
+            {
+                Id = 1,
+                Name = "SuperMan",
+                FirstName = "Clark",
+                LastName = "KENT",
+                Place = "Kansas"
+            },
+            new SuperHero
+            {
+                Id = 2,
+                Name = "Iron Man",
+                FirstName = "Tony",
+                LastName = "STARK",
+                Place = "California"
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return NotFound("Hero Not Found");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+                return NotFound("Hero Not Found");
+
+            hero.Name = request.Name;  
+            hero.FirstName = request.FirstName;
+            hero.LastName  = request.LastName;
+            hero.Place = request.Place;
+            return Ok(heroes);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SuperHero>> Delete(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return NotFound("Hero Not Found");
+            heroes.Remove(hero);
+            return Ok(heroes);
+        }
+    }
+}
---
+++ Week2AssigmentEFCoreApi/EFCoreApiCodeFirstAPI/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreApiCodeFirst.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(option)
+        {
+
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
